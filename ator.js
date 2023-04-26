7e2b1a115692ec91cbcd12e34fd607ce8a4ac2fb
innyti <116384655+innyti@users.noreply.github.com>
2023-04-26 17:42:14 -0300

    Add files via upload

---
+++ ator.js
@@ -0,0 +1,59 @@
+//código que mostra o ator
+let xAtor = 85;
+let yAtor = 366;
+let colisao = false;
+let meusPontos = 0;
+
+
+function mostraAtor(){
+  image(imagemDoAtor, xAtor, yAtor, 30, 30)
+}
+
+function movimentaAtor(){
+  if (keyIsDown(UP_ARROW)){
+    yAtor -= 3
+  }
+  if (keyIsDown(DOWN_ARROW)){
+    if(podeSeMover())    
+      yAtor += 3;
+  }
+}
+
+
+function verificaColisao(){
+  for (let i = 0; i < imagemCarros.length; i++){
+    colisao = collideRectCircle(xCarros[i], yCarros[i], comprimentoCarro, alturaCarro, xAtor + 15, yAtor + 15, 15)
+    if (colisao){
+      voltaAtorParaPosicaoInicial();
+    if (sePontosMaiorQueZero()){  
+      meusPontos -= 1;
+      }
+    }
+  }
+}
+
+function voltaAtorParaPosicaoInicial(){
+  yAtor = 366;
+}
+
+function incluiPontos(){
+  textAlign(CENTER)
+  textSize(25)
+  fill(color(241, 196, 15))
+  text(meusPontos, width / 5, 27)
+}
+
+function marcaPonto(){
+  if(yAtor < 15){
+    meusPontos += 1;
+    voltaAtorParaPosicaoInicial();
+  }
+}
+
+function sePontosMaiorQueZero(){
+  return meusPontos > 0
+}
+
+function podeSeMover(){
+  return yAtor < 366;
+}
